27a285fb92ea14d184878f277fcf4d08c8d1879c
Romeo Girard <romeo.girard@outlook.com>
2020-11-20 08:49:49 -0500

    Création du jeu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+// Le jeu
+
+    class Jeu{
+        constructor(){
+            console.log("Création du jeu");
+        }
+    }
+
+// Le serpent
+
+    class Serpent{
+        constructor() {
+            console.log("Création du serpent");
+        }
+    }
+
+
+// La pomme
+
+    class Pomme{
+        constructor() {
+            console.log("Création de la pomme");
+        }
+    }
+
+    var unePartie = new Jeu();
+
 
 });
